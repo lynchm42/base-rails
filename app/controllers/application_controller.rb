764a4f7cb1fdaa58dd8ced8eabdf02ddf47d1245
lynchm42 <lynchm@uchicago.edu>
2023-03-10 01:17:04 +0000

    design getting there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,7 @@ class ApplicationController < ActionController::Base
 
   def home
 
-   render({ :template => "home/home.html.erb" })
-  end 
-
-  def home_1
-
     render({ :template => "home/home_1.html.erb" })
    end 
 
-   def home_2
-
-    render({ :template => "home/home_2.html.erb" })
-   end 
-
-   def home_3
-
-    render({ :template => "home/home_3.html.erb" })
-   end 
-
-   def home_4
-
-    render({ :template => "home/home_4.html.erb" })
-   end 
 end
